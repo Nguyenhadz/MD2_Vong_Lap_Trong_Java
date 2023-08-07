58248ee54054d685b270625ffd9e263fc79cabf6
Nguyenhadz <nguyenha210895@gmail.com>
2023-08-07 10:55:00 +0700

    Commit

--- src/drawing_menu/Main.java
+++ src/drawing_menu/Main.java
@@ -33,7 +33,7 @@ public class Main {
                 System.out.println("* * * * *");
                 System.out.println("* * * * *");
                 System.out.println("* * * * *");
-                System.out.println("* * * * *");
+                System.out.println("* * * * @");
                 break;
             case 0:
                 System.exit(0);
