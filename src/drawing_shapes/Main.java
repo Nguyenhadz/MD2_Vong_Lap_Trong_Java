b1c5f1de6f4e61a19fcbc8f88b977a28343ad15e
Nguyenhadz <nguyenha210895@gmail.com>
2023-08-07 11:04:02 +0700

    Commit

---
+++ src/drawing_shapes/Main.java
@@ -0,0 +1,8 @@
+package drawing_shapes;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Muahaha");
+    }
+}
